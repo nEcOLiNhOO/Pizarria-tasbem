eee6a7418b4b448b5fd2b8da83588f508e7fadb6
nEcOLiNhOO <vasco.nunes42@gmail.com>
2023-06-18 00:05:56 +0100

    updates index,sobre_nos

---
+++ html/scripts/script1.js
@@ -0,0 +1,22 @@
+document.addEventListener('DOMContentLoaded', function() {
+    var video = document.querySelector('.video-slideshow');
+    var fraseElement = document.createElement('p');
+    fraseElement.textContent = 'Delicie-se com nossas pizzas artesanais, feitas com paixão e ingredientes frescos. Uma fatia de sabor que irá encantar seu paladar e alimentar sua alma';
+    fraseElement.classList.add('frase');
+    fraseElement.classList.add('fade-in');
+    fraseElement.classList.add('slide-in');
+  
+    video.addEventListener('ended', function() {
+      video.pause();
+      video.parentNode.insertBefore(fraseElement, video.nextSibling);
+      document.querySelector('.imagem_fundo_frase').classList.add('imagem_fundo_frase-visible');
+  
+      setTimeout(function() {
+        video.currentTime = 0;
+        video.play();
+        fraseElement.remove();
+        document.querySelector('.imagem_fundo_frase').classList.remove('imagem_fundo_frase-visible');
+      }, 11000);
+    });
+  });
+  
